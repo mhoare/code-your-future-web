ace5e54534231cbe88f40dfd2edda6f182f52484
Pedro Guimaraes <pedro.guima@gmail.com>
2017-02-05 22:03:12 +0000

    testing automatic demo deployment

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DATE=$(date +%Y%m%d_%H%M%s)
+BRANCH="$1"
+
+
+if [ "$#" -ne 1 ]; then
+	echo "Please provide an environment (demo, staging, production)"
+	exit 1
+fi
+
+if [ $BRANCH == "demo" ]; then
+	REPO_NAME="web-static-demo"
+elif [ $BRANCH == "staging" ]; then
+	REPO_NAME="web-static-staging"
+elif [ $BRANCH == "production" ]; then
+	REPO_NAME="web-static-production"
+else
+	echo "Please provide a valid branch"
+	exit 1
+fi
+
+git clone git@github.com:Code-Your-Future/$REPO_NAME.git
+cd $REPO_NAME
+git rm *
+cp -a ../build/* .
+git config --global user.email "admin@codeyourfuture.co"
+git config --global user.name "Automated bot"
+git add .
+git commit -m "Rebuild on $DATE"
+git push
+
